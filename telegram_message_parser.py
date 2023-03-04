099e89060115515de9c261d4bb2b1b1a24c4711f
SaneOne <SaneOne1995@gmail.com>
2023-03-04 22:06:13 +0300

    Update telegram_message_parser.py

--- telegram_message_parser.py
+++ telegram_message_parser.py
@@ -91,17 +91,12 @@ class TelegramMessageParser:
 
     # voice message in private chat, speech to text with Whisper API and process with ChatGPT
     async def chat_voice(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        message = update.message 
- 
-    # Проверяем, является ли сообщение голосовым и является ли оно ответом на сообщение бота 
-    if message.voice and message.reply_to_message and message.reply_to_message.from_user.id == self.bot_id: 
- 
-        # Обработка голосового сообщения и отправка ответа 
-        ... 
-         
-    else:
-        pass 
-        # Игнорирование других голосовых сообщений
+        # check if it's a private chat
+        #if not update.effective_chat.type == "private":   
+        # return 
+    
+
+
         
         # check if user is allowed to use this bot
         if not self.check_user_allowed(str(update.effective_user.id)):
