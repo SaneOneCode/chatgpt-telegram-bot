505e255b2b25b54fc9fd79059d87c4226edce7f5
SaneOne <SaneOne1995@gmail.com>
2023-03-04 23:06:12 +0300

    Update telegram_message_parser.py

--- telegram_message_parser.py
+++ telegram_message_parser.py
@@ -91,21 +91,17 @@ class TelegramMessageParser:
 
     # voice message in private chat, speech to text with Whisper API and process with ChatGPT
     async def chat_voice(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        # check if it's a private chat
-        #if not update.effective_chat.type == "private":
-        #    return
+    #check if it's a private chat
+        if not update.effective_chat.type == "private":
+            return
         
         # check if user is allowed to use this bot
         async def chat_voice(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-            message = update.message 
-        # Проверяем, является ли сообщение голосовым и является ли оно ответом на сообщение бота 
-            if message.voice and message.reply_to_message: 
-        # Обработка голосового сообщения и отправка ответа 
-                    await context.bot.send_voice(chat_id=update.message.chat_id, voice=update.message.voice)   
-            else: 
-        # Игнорирование других голосовых сообщений 
-                    pass
-
+            if not self.check_user_allowed(str(update.effective_user.id)):
+                await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Ты не имеешь права отправлять Членману Голосовые сообщения, обратись к @SaneOne для получения прав.")
+        return
         # sending typing action
         await context.bot.send_chat_action(
             chat_id=update.effective_chat.id,
